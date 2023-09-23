7388c318ee16060727f147fe45992d5e42edf72b
Obada Moustafa Sharbatly <obadamoustafa@gmail.com>
2023-09-23 20:45:17 +0200

    toml file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-
+const serverless = require("serverless-http");
 // Define your routes and controllers here
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -14,3 +14,5 @@ app.get("/api/test", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
 });
+
+module.exports.handler = serverless(app);
